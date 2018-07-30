d85180e107ff3ba41c4262be7ae11f35fcad34b1
syahruldzikri <41882942+syahruldzikri@users.noreply.github.com>
2018-07-30 12:46:09 +0800

    navi

--- App.js
+++ App.js
@@ -37,10 +37,25 @@ _click = () => {
 
 
 
-export default createDrawerNavigator({
-  Home : {screen : App},
-  Counter : {screen : Pegipage},
-  Vision : {screen : Lol},
+- import { DrawerNavigator } from 'react-navigation';
++ import { DrawerNavigator, DrawerItems } from 'react-navigation';
+
+const App = DrawerNavigator({
+  Home: {
+    screen: MyHomeScreen,
+  },
+  Notifications: {
+    screen: MyNotificationsScreen,
+  },
+-}
++}, {
++  contentComponent: (props) => (
++    <View>
++      <Text>Custom Header</Text>
++      <DrawerItems {...props} />
++      <Text>Custom Footer</Text>
++    </View>
++  )
 });
 const styles = StyleSheet.create({
   container: {
