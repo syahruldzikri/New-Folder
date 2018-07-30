cccb46aa7ae1860b4d8f1a7ebfa9f880c750e94a
syahruldzikri <41882942+syahruldzikri@users.noreply.github.com>
2018-07-30 18:10:52 +0800

    update

--- data/Test Folder/my test/ui/login.js
+++ data/Test Folder/my test/ui/login.js
@@ -1,18 +1,3 @@
-class UIDesign extends React.Component {
-  render() {
-    return (
-      <View style={styles.container2}>
-        <Text>MAIN PAGE</Text> ///main page design for top
-        <View style={styles.container3}>
-          <Text>Search</Text> ///for google map search
-      </View>
-    )
-  }
-}
-
-
-
-///another type of page UIDesign
 import React from 'react';
 import { Text, View } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation';
