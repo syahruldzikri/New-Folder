10f3bd9960c4d0f9118daeccb7b1a4a9e301fd03
aimannajmi <aimannajmi@users.noreply.github.com>
2018-07-30 19:43:39 +0800

    firebase in progress

--- Test/firebaseRead.js
+++ Test/firebaseRead.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert, Image, Textinput, Picker } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, Image, TextInput, Picker } from 'react-native';
+import * as firebase from 'firebase';
 
 class Apps extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  _click = () => {
+var database = firebase.database();
+database.ref('user/').set({
+  name:'aiman',
+
+})
+    //Alert.alert("Thank you", "Your information has been recorded");
+  }
+
   render(){
     return(
       <View>
-      <Text>hello world</Text>
+      <TextInput/>
+      <Button title= "Sent Data" onPress = {this._click}/>
       </View>
     )
   }
