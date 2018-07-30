ce82ad41a6b69e70282808f74d3900a94bd7cca2
syahruldzikri <41882942+syahruldzikri@users.noreply.github.com>
2018-07-30 17:10:18 +0800

    logo

---
+++ data/Test Folder/my test/ui/main page design.js
@@ -0,0 +1,9 @@
+class UIDesign extends React.Component {
+  render() {
+    return (
+      <View style={styles.container2}>
+        <Text>This is the UI/UX design</Text>
+      </View>
+    )
+  }
+}
