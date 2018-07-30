4ee78a774d693881c8600e9a6a606d6f1086c80a
syahruldzikri <41882942+syahruldzikri@users.noreply.github.com>
2018-07-30 18:32:03 +0800

    Merge branch 'master' of https://github.com/syahruldzikri/New-Folder

--- data/Test Folder/my test/ui/main page design.js
+++ data/Test Folder/my test/ui/main page design.js
@@ -1,10 +1,34 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { creattopTabNavigator } from 'react-navigation';
+
 class UIDesign extends React.Component {
   render() {
     return (
+
       <View style={styles.container2}>
-        <Text>MAIN PAGE</Text> ///main page design for top
-        <View style={styles.container3}>
+        <Text>title</Text> ///main page design for top
+        
+class HomeScreen extends React.Component {
+          render() {
+            return (
+              <View style={{ flex: 1, justifyContent: 'left', alignItems: 'left' }}>
+                <Text>Home!</Text>
+              </View>
+            );
+          }}
+
+         <View style={styles.container2}>
           <Text>Search</Text> ///for google map search
+
+class HomeScreen extends React.Component {
+                    render() {
+                      return (
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Home!</Text>
+          </View>
+                      );
+                    }}
       </View>
     )
   }
@@ -13,31 +37,27 @@ class UIDesign extends React.Component {
 
 
 ///another type of page UIDesign
-import React from 'react';
-import { Text, View } from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation';
+///import React from 'react';
+///import { Text, View } from 'react-native';
+//import { creattopTabNavigator } from 'react-navigation';
 
-class HomeScreen extends React.Component {
+//class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, justifyContent: 'left', alignItems: 'left' }}>
         <Text>Home!</Text>
       </View>
     );
   }
 }
 
-class SettingsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
-      </View>
-    );
-  }
-}
+//class SettingsScreen extends React.Component {
+  //render() {
+    //return (
+      //<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        //<Text>Settings!</Text>
+      //</View>);}}
 
-export default createBottomTabNavigator({
-  Home: HomeScreen,
-  Settings: SettingsScreen,
-});
+//export default createBottomTabNavigator({
+  //Home: HomeScreen,
+  //Settings: SettingsScreen,});
