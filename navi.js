6f58d99cc3c37719d9425b41f1f7f0c4a422bdd5
syahruldzikri <41882942+syahruldzikri@users.noreply.github.com>
2018-07-30 12:41:41 +0800

    navi page

--- navi.js
+++ navi.js
@@ -1,1 +1,20 @@
-{}
+- import { DrawerNavigator } from 'react-navigation';
++ import { DrawerNavigator, DrawerItems } from 'react-navigation';
+
+const App = DrawerNavigator({
+  Home: {
+    screen: MyHomeScreen,
+  },
+  Notifications: {
+    screen: MyNotificationsScreen,
+  },
+-}
++}, {
++  contentComponent: (props) => (
++    <View>
++      <Text>Custom Header</Text>
++      <DrawerItems {...props} />
++      <Text>Custom Footer</Text>
++    </View>
++  )
+});
