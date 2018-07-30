a2316f3e1a81817a12e995af627f94de30c72c6c
syahruldzikri <41882942+syahruldzikri@users.noreply.github.com>
2018-07-30 17:54:55 +0800

    sample page design

--- data/Test Folder/my test/ui/main page design.js
+++ data/Test Folder/my test/ui/main page design.js
@@ -2,7 +2,9 @@ class UIDesign extends React.Component {
   render() {
     return (
       <View style={styles.container2}>
-        <Text>This is the UI/UX design</Text>
+        <Text>MAIN PAGE</Text> ///main page design for top
+        <View style={styles.container3}>
+          <Text>Search</Text> ///for google map search
       </View>
     )
   }
